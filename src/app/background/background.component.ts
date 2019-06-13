6b66f75745a840449f3956d2d020cde1ff9f24c1
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2019-06-13 04:03:14 -0500

    made some changes to everything

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -6,7 +6,10 @@ import * as $ from 'jquery';
   styleUrls: ['./background.component.scss']
 })
 export class BackgroundComponent implements OnInit {
-
+  images = [`https://www.wallpapermaiden.com/wallpaper/6509/download/2560x1440/web-development-php-coding-classes.jpg`, 'https://www.wallpapermaiden.com/wallpaper/33782/download/2560x1440/forest-mist-trees-long-road-nature.jpeg'
+          , 'https://www.wallpapermaiden.com/wallpaper/33501/download/2560x1440/turkish-coffee-camera-drinks-top-view-cup-food.jpeg'];
+  
+  //images = [`assets/img/helicopter1.jpg`, 'assets/img/helicopter2.jpg', 'assets/img/helicopter2.jpg'];
   constructor() { }
 
   ngOnInit() {
--- src/app/careers/careers.component.ts
+++ src/app/careers/careers.component.ts
@@ -8,15 +8,23 @@ import * as $ from 'jquery';
 })
 export class CareersComponent implements OnInit {
 
+
+  aboutme = "Ben has graudated from the University of Missouri - Columbia (UMC) in May 2019 with B.S. Computer Engineering Degree and minor in Math and Computer Science. " +
+          "Before graduation, he was working as a Part-time Student Software Developer at College of Engineering and School of Natural Resources, "+
+          "and he was also a Peer Learning Assistant where he had an oppotunity to help his fellows at school to perform beyond the standards in computer and math courses. During his time as a software developer at UMC, "+
+          "he had contributed his work to many meaningful projects that help enhancing people lives. These lives include students, faculties, other researches, and farmers.";
+
   constructor() { }
 
   ngOnInit() {
+    
     $(document).ready(function(){
       $("#fadeUpHeader").animate({ opacity: 1, top: "-50px" }, "slow","swing");
     });
     $(document).ready(function(){
       $("#fadeUpContent").animate({ opacity: 1, top: "-70px" },"slow","linear");
     });
+    
   }
 
 }
