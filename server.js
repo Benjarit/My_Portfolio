6cff3808dfa302c6301da4a35b6d788399b7f116
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2019-06-17 17:57:06 -0500

    fixed server

--- server.js
+++ server.js
@@ -8,23 +8,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(__dirname + '/dist/my-portfolio'));
 
-console.log("Hey, This is from server.");
-
 const client = new Client({
   connectionString: 'postgres://xobkypwdmxwnxx:2af823a29661ed2051fdf9c7339ed08d0dea08dc3203987d40da275842dd3f42@ec2-23-21-91-183.compute-1.amazonaws.com:5432/d59f1gjjltj1so',
   ssl: true,
 });
 client.connect();
-client.query('SELECT * FROM public.job;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
 
 app.get('/api/forms', (req, res) => {
-    res.send("api works")
+    client.query('SELECT * FROM public.job;', (err, result) => {
+        if (err) throw err;
+        res.json(result);
+        client.end();
+    });
 })
 
 app.get('/*', function(req, res) {
