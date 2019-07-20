de8a3e1cbc1531f65f6152318bf1d7ab1b11fe08
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2019-07-19 18:07:28 -0700

    checking

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ var saltHashPassword = function (userpassword) {
 }
 app.post('/api/login', (req, res) => {
     var passwordData = saltHashPassword(req.body.password);
+    res.send({user:String(req.body.usernam), password: passwordData.passwordHash, salt: passwordData.salt});
 
     // client.query('INSERT INTO public."user" (username, password, salt) VALUES ($1, $2, $3)',[req.body.username,passwordData.passwordHash, passwordData.salt], (err, results) => {
     //     if (err){
